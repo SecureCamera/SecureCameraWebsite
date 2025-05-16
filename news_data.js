3c7b0bc116048b90ef3ca41c422175be54aa7933
Adam Brown <adamwbrown@gmail.com>
2025-05-15 19:47:33 -0700

    Added more news stories

--- news_data.js
+++ news_data.js
@@ -43,10 +43,45 @@ const newsData = [
         header_image: "images/news/news_header_6.webp"
     },
     {
+        title: "CBP Stores Data from Thousands of Phones Searched at Border",
+        description: "US Customs and Border Protection has been copying texts, photos, and more from travelers' phones without warrants, storing the data for 15 years.",
+        link: "https://www.theregister.com/2022/09/15/wyden_cbp_phone_database/",
+        logo_image: "images/news/news_logo_theregister.png",
+        header_image: "images/news/news_header_bordersearch.jpg"
+    },
+    {
         title: "iOS 18's 'Enhanced Visual Search' Shares Photos by Default",
         description: "This sneaky iOS 18 setting shares your photos with Apple",
         link: "https://nypost.com/2024/12/31/tech/sneaky-ios-18-setting-shares-photos-with-apple-how-to-turn-it-off/",
         logo_image: "images/news/news_logo_newyorkpost.png",
         header_image: "images/news/news_header_7.webp"
-    }
+    },
+    {
+        title: "CHP Officers Stole and Shared Women's Nude Photos",
+        description: "California Highway Patrol officers were caught stealing and sharing nude photos from female arrestees' phones.",
+        link: "https://abc30.com/chp-sean-harrington-nude-pictures-photos/366690/",
+        logo_image: "images/news/news_logo_abc.png",
+        header_image: "images/news/news_header_chp.jpg"
+    },
+    {
+        title: "Police in MENA Target LGBTQ+ Individuals Using Phone Photos",
+        description: "Security forces in MENA countries use seized phone photos to arrests, torture, and prosecutions LGBTQ+ people. Seized photos were then used to entrap others.",
+        link: "https://www.hrw.org/report/2023/02/21/all-terror-because-photo/digital-targeting-and-its-offline-consequences-lgbt",
+        logo_image: "images/news/news_logo_hrw.png",
+        header_image: "images/news/news_header_mena_lgbtq.jpg"
+    },
+    {
+        title: "Samsung Bug Sends Gallery Photos to Random Contacts",
+        description: "A Samsung Messages app bug caused phones to send photos to random contacts without user consent or notification.",
+        link: "https://www.theguardian.com/technology/2018/jul/03/samsung-galaxy-s8-s9-phones-sent-photos-random-contacts",
+        logo_image: "images/news/news_logo_guardian.png",
+        header_image: "images/news/news_header_samsung_bug.jpg"
+    },
+    {
+        title: "Google Takeout Bug Sent Users Private Videos to Strangers",
+        description: "Google Photos users received private videos from strangers due to a Takeout bug mixing up exported archives.",
+        link: "https://www.bbc.com/news/technology-51375350",
+        logo_image: "images/news/news_logo_bbc.png",
+        header_image: "images/news/news_header_google_takeout.webp"
+    },
 ];
